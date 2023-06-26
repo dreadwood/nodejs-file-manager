869af010a958da3028a6781f924fe3ceaa54e79c
srg <skabassjob@gmail.com>
2023-06-26 21:05:23 +0400

    feat: add message module

---
+++ src/modules/message.js
@@ -0,0 +1,35 @@
+import { Colors, getColorStr } from './color.js'
+
+export const message = {
+  goodbye(EOL, username) {
+    console.log(
+      `${EOL}Thank you for using File Manager, ${
+        username ? `${getColorStr(username, Colors.fgBlue)}, ` : ''
+      }goodbye!`
+    )
+  },
+
+  welcome(username) {
+    console.log(
+      `Welcome to the File Manager${
+        username ? `, ${getColorStr(username, Colors.fgBlue)}` : ''
+      }!`
+    )
+  },
+
+  hotkeys() {
+    console.log('Press control-D, ^-D, Ctrl-D to exit')
+  },
+
+  directory(cwd) {
+    return `You are currently in ${getColorStr(cwd, Colors.fgBlue)} > `
+  },
+
+  inputError() {
+    console.log(getColorStr('Invalid input', Colors.fgRed))
+  },
+
+  operationError() {
+    console.log(getColorStr('Operation failed', Colors.fgRed))
+  },
+}
