56f3580414a2ba3e39399a04ca87a96c8f5f845d
srg <skabassjob@gmail.com>
2023-06-26 22:45:05 +0400

    feat: create readline interface

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('How Do You Do, Fellow Kids?')
+import process from 'node:process'
+import os from 'node:os'
+import { getUsername } from './modules/username.js'
+import { message } from './modules/message.js'
+import { readlineInput } from './readline-input.js'
+
+const username = getUsername()
+
+process.chdir(os.homedir())
+process.on('exit', () => message.goodbye(os.EOL, username))
+process.on('SIGINT', process.exit)
+
+message.welcome(username)
+message.hotkeys()
+
+readlineInput()
---
+++ src/readline-input.js
@@ -0,0 +1,35 @@
+import readline from 'node:readline/promises'
+import { osHandler } from './commands/index.js'
+import { parser } from './modules/parser.js'
+import { message } from './modules/message.js'
+import { ExitCode, Commands } from './constants.js'
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+export const readlineInput = async () => {
+  const input = await rl.question(message.directory(process.cwd()))
+
+  const [inputCommand, firstArguments, secondArguments] = parser(input)
+
+  try {
+    switch (inputCommand) {
+      case Commands.exit:
+        process.exit(ExitCode.success)
+
+      case Commands.os:
+        osHandler(firstArguments)
+        break
+
+      default:
+        message.inputError()
+        break
+    }
+  } catch {
+    message.operationError()
+  }
+
+  readlineInput()
+}
