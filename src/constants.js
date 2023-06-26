405b5087107187305d48dd6f40b7862866f76fc7
srg <skabassjob@gmail.com>
2023-06-26 21:04:54 +0400

    feat: add constant

---
+++ src/constants.js
@@ -0,0 +1,24 @@
+export const USER_ARGV_INDEX = 2;
+export const PREFIX_CLI_COMMAND = '--username=';
+
+export const ExitCode = {
+  error: 1,
+  success: 0,
+};
+
+export const Commands = {
+  exit: '.exit',
+  up: 'up',
+  cd: 'cd',
+  ls: 'ls',
+  cat: 'cat',
+  add: 'add',
+  rn: 'rn',
+  cp: 'cp',
+  mv: 'mv',
+  rm: 'rm',
+  os: 'os',
+  hash: 'hash',
+  compress: 'compress',
+  decompress: 'decompress',
+};
